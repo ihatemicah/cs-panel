a548f551737f24cd948ddfc2112aa41f98417e06
Micah Carroll <micahgester@gmail.com>
2016-11-30 17:48:45 -0800

    changed slider

---
+++ default.js
@@ -0,0 +1,7 @@
+  var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 6,
+        paginationClickable: true,
+        spaceBetween: 30,
+        keyboardControl: true,
+    });
